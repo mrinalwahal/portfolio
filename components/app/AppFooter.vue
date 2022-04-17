a9b13ce9c3fdb4558c85159956b09ea6ffb2fac0
Mrinal Wahal <mrinal_w@rocketmail.com>
2022-04-17 21:04:01 +0530

    chore: shifted location to settings

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20">
     <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
-      <div class="flex items-end">
+      <div class="flex items-end" v-if="settings.location">
           <div v-html="$feathericons['map-pin'].toSvg()" class="mr-2"></div>
-          Remote | Delhi, India
+          {{settings.location}}
     </div>
       <div class="flex items-center space-x-4">
         <AppLangSwitcher />
@@ -14,8 +14,14 @@
 </template>
 
 <script>
+
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
+    ...mapGetters([
+      'settings',
+    ]),
     availableLocales () {
       return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
     }
